36ed1423ea73bce20c6d4f7b9c27c5cba49c3927
UttamSaha <uttamsaha982@gmail.com>
2022-04-24 16:24:02 +0600

    post method implemented

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
+require("dotenv").config(); //to use env variable
 
 //middleware
 app.use(cors());
@@ -40,6 +40,14 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
+
+    //POST // insert data
+    app.post('/service', async(req,res)=>{
+        const newService = req.body;
+
+        const result = await serviceCollection.insertOne(newService);
+        res.send(result);
+    })
   } finally {
   }
 }
