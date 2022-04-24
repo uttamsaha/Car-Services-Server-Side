6357191a88b955154261855a6a78940c0c8bd5d0
UttamSaha <uttamsaha982@gmail.com>
2022-04-24 17:02:38 +0600

    updated

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ async function run() {
 
         const result = await serviceCollection.insertOne(newService);
         res.send(result);
+    });
+
+    //DELTE singel post
+    app.delete('/service/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result);
     })
   } finally {
   }
