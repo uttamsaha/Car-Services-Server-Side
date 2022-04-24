c1f5dd088c975ac924139e4ddd6ed10ee06df597
UttamSaha <uttamsaha982@gmail.com>
2022-04-24 13:17:50 +0600

    read operation done

--- index.js
+++ index.js
@@ -10,6 +10,41 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+//mongodb connection
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6vrwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service"); //db & table connect
+
+    //read all data
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //load /read single data id wise /server/id te service pabo server side e client side e seta load korbo..
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir); //calling run function
+
 app.get("/", (req, res) => {
   res.send("Running Car services server");
 });
